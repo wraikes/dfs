08e06e11778daabea394668c9acb47cb61b3ff56
wraikes <wraikes@hotmail.com>
2019-07-15 17:03:23 +0000

    Fix table data pull

--- data_prep/train_model/scrape_nascar_data.py
+++ data_prep/train_model/scrape_nascar_data.py
@@ -12,56 +12,62 @@ class DataPull:
         self.prediction = prediction
         self.pid = pid
         
+        self._start_pid = 209 if self.train else self.pid
         self._json_data = []
         self._table_data = []
         self._owner_data = []
         self._final_data = []
     
     def pull_json_data(self):
-        if self.train:
-            _start_pid = 209
-        else:
-            _start_pid = self.pid
-            
-        for _pid in range(_start_pid, self.pid+1):
-            _html = 'https://www.linestarapp.com/DesktopModules/DailyFantasyApi/API/Fantasy/GetSalariesV4?sport=9&site=2&periodId={}'.format(_pid)
-            _page = requests.get(_html).content.decode()   
-            self._json_data.append(json.loads(_page))   
+
+        for pid in range(self._start_pid, self.pid+1):
+            html = 'https://www.linestarapp.com/DesktopModules/DailyFantasyApi/API/Fantasy/GetSalariesV4?sport=9&site=2&periodId={}'.format(pid)
+            page = requests.get(html).content.decode()   
+            self._json_data.append(json.loads(page))   
     
     
     def extract_matchupdata(self):
         
-        for _json in self._json_data:
-            _table_values = dict()
+        for race in self._json_data:
+            tables = dict()
 
-            for i in range(len(data['MatchupData'])):
-                col_names = data['MatchupData'][i]['Columns']
+            for i, table in enumerate(race['MatchupData']):
+                col_names = table['Columns']
+                id_store = []
             
-                for player_data in data['MatchupData'][i]['PlayerMatchups']:
-                    player_id = player_data['PlayerId']
-                    player_dict = dict(zip(col_names, player_data['Values']))
+                for player in table['PlayerMatchups']:
+                    player_id = player['PlayerId']
+
+                    if player_id in id_store:
+                        continue
+                    else:
+                        id_store.append(player_id)
+                        
+                    player_dict = dict(zip(col_names, player['Values']))                    
                     
-                    if player_id not in _table_values.keys():
-                        _table_values[player_id] = player_dict
+                    if player_id not in tables.keys():
+                        tables[player_id] = player_dict
+                        
                     else:
                         for key, value in player_dict.items():
-                            if _table_values[player_id][key]:
-                                table_values[player_id]['{}_{}'.format(key, i] = value
-                            else:
-                                _table_values[player_id][key] = value
+                            if key in tables[player_id].keys():
+                                tables[player_id]['{}_{}'.format(key, i)]= value
                                 
-            self._table_data.append(_table_values)
+                            else:
+                                tables[player_id][key] = value
+                                
+            self._table_data.append(tables)
     
     
     def extract_owner_data(self):
         
-        for _data in self._json_data:
-            owner_values = []
+        for race in self._json_data:
+            owner_data = []
                 
             for player_data in data['Ownership']['Salaries']:
-                owner_values.append(player_data)
+                owner_data.append(player_data)
                 
-            self._owner_data.append(owner_values)
+            self._owner_data.append(owner_data)
         
         
     def final_data(self):
