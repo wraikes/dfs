d6a616d58b95c6094d3ed5df1708a971274f9b83
wraikes <wraikes@hotmail.com>
2019-07-19 03:21:40 +0000

    Reload get_model files

---
+++ dfs-nascar/get_model/build_model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from joblib import dump
+from sklearn.metrics import mean_squared_error
+
+class Model:
+    
+    def __init__(self, model, params):
+        self.model = model
+        self.params = params
+        self.predictions = None
+        self.performance = None
+
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train.drop(columns='name'), y_train)
+    
+
+    def test(self, X_test, y_test):
+        X_test['preds'] = self.model.predict(X_test.drop(columns='name'))
+        self.performance = mean_squared_error(y_test, X_test['preds'])**0.5
+        self.predictions = X_test[['name', 'salary', 'preds']]
+        
+    
+    def save(self, path):
+        joblib.dump(self.model, path)
+
+        
---
+++ dfs-nascar/get_model/load_data.py
@@ -0,0 +1,59 @@
+#load rds dataset
+import psycopg2
+import configparser
+import pandas as pd
+import numpy as np
+
+
+class LoadData:
+    def __init__(self, config_file, db_name):
+        self.config_file = config_file
+        self.db_name = db_name
+        self.df = []
+        self.cursor = None
+
+
+    def setup_connection(self):
+        cfg = configparser.ConfigParser()
+        cfg.read(self.config_file)
+    
+        dbname = cfg['PGCONNECT']['dbname']
+        host = cfg['PGCONNECT']['host']
+        port = cfg['PGCONNECT']['port']
+        user = cfg['PGCONNECT']['user']
+        password = cfg['PGCONNECT']['password']
+    
+        conn = psycopg2.connect(
+            dbname=dbname, 
+            host=host, 
+            port=port, 
+            user=user, 
+            password=password
+        )
+        
+        conn.autocommit = True
+        self.cursor = conn.cursor()
+
+
+    def create_df(self):
+        
+        self.cursor.execute('SELECT * FROM {}'.format(self.db_name))
+        for row in self.cursor.fetchall():
+            self.df.append(row)
+    
+        self.df = pd.DataFrame(self.df)
+        self.df.columns = [self.cursor.description[i][0] for i in range(len(self.cursor.description))]
+
+     
+    def data_clean(self, non_numeric_cols, predictors, label=['ps']):
+        self.df = self.df[label+predictors].dropna() 
+
+        for col in self.df.columns:
+            if col not in non_numeric_cols:
+                self.df[col] = pd.to_numeric(self.df[col])
+    
+        self.df['_name'] = self.df['name']            
+        self.df = pd.get_dummies(self.df, columns=['_name', 'restrictor_plate', 'surface'], drop_first=True)
+        self.df['race_date'] = pd.to_datetime(self.df['race_date']).dt.date  
+    
+        return self.df
