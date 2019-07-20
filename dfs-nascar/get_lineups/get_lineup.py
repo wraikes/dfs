092410366f8df6ee3d0fc2937d6903600302f087
wraikes <wraikes@hotmail.com>
2019-07-20 00:54:14 +0000

    Get predictions ready

--- dfs-nascar/get_lineups/get_lineup.py
+++ dfs-nascar/get_lineups/get_lineup.py
@@ -1,6 +1,15 @@
+import pandas as pd
+import configparser
 from joblib import load
-from get_data.scrape_nascar_data import NascarDataPull
-from get_model.optimizer import Optimizer
+from scrape_nascar_data import NascarDataPull
+from optimizer import Optimizer
+
+cfg = configparser.ConfigParser()
+cfg.read('build_model.ini')
+
+label = eval(cfg['TABLEVARIABLES']['label'])
+predictors = eval(cfg['TABLEVARIABLES']['predictors'])
+non_numeric_cols = eval(cfg['TABLEVARIABLES']['non_numeric_cols'])
 
 df = NascarDataPull(train=False, pid=258)
 df.pull_json_data()
@@ -9,71 +18,41 @@ df.extract_table_data()
 
 df = df._final_data.copy()
 df = pd.DataFrame.from_dict(
-    {(i, j, k): df[i][j][k] for i in df.keys() for j in df[i].keys() for k in df[i][j][k].keys()},
+    {(i, j): df[i][j] for i in df.keys() for j in df[i].keys()},
     orient='index'
 )
 
-
-def data_clean(non_numeric_cols, predictors):
+def data_clean(df, non_numeric_cols, predictors):
+    
     df = df[predictors].dropna() 
-
+    
     for col in df.columns:
         if col not in non_numeric_cols:
-            df[col] = pd.to_numeric(df[col])
-
-    df['_name'] = df['name']            
-    df = pd.get_dummies(df, columns=['_name', 'restrictor_plate', 'surface'], drop_first=True)
-    df['race_date'] = pd.to_datetime(df['race_date']).dt.date  
-
+            try:
+                df[col] = pd.to_numeric(df[col])
+            except:
+                print(col)
+    
     return df
-
-non_numeric_cols = ['race_date', 'name', 'restrictor_plate', 'surface']
-predictors = [
-    'name',
-    'salary',
-    'race_date',
-    'pp',
-    'races',
-    'wins',
-    'top_fives',
-    'top_tens',
-    'avg_finish',
-    'laps_led_race',
-    'fastest_laps_race',
-    'avg_pass_diff',
-    'quality_passes_race',
-    'fppg',
-    'practice_laps',
-    'practice_best_lap_time',
-    'practice_best_lap_speed',
-    'qualifying_pos',
-    'qualifying_best_lap_time',
-    'qualifying_best_lap_speed',
-    'laps',
-    'miles',
-    'surface',
-    'restrictor_plate',
-    'cautions_race',
-    'races_3',
-    'finished',
-    'wins_3',
-    'top_5s',
-    'top_10s',
-    'avg_place',
-    'races_4',
-    'finished_4',
-    'wins_4',
-    'top_5s_4',
-    'top_10s_4',
-    'avg_place_4'
-]
 
 
 df = data_clean(df, non_numeric_cols, predictors)
+df.columns = [
+    's', 'pid', 'name', 'pos', 'salary', 'gid', 'gi', 'race_date', 
+    'ppg', 'pp', 'ps', 'ss', 'stat', 'is_', 'notes', 'floor', 'ceil', 'conf', 'ptid', 'otid', 
+    'htid', 'oe', 'opprank', 'opptotal', 'dspid', 'dgid', 'img', 'pteam', 'hteam', 'oteam', 
+    'lock', 'id', 'races', 'wins', 'top_fives', 'top_tens', 'avg_finish', 
+    'laps_led_race', 'fastest_laps_race', 'avg_pass_diff', 'quality_passes_race', 
+    'fppg', 'practice_laps', 'practice_best_lap_time', 'practice_best_lap_speed', 
+    'qualifying_pos', 'qualifying_best_lap_time', 'qualifying_best_lap_speed', 
+    'laps', 'miles', 'surface', 'restrictor_plate', 'cautions_race', 'races_3', 
+    'finished', 'wins_3', 'top_5s', 'top_10s', 'avg_place', 'races_4', 'finished_4', 
+    'wins_4', 'top_5s_4', 'top_10s_4', 'avg_place_4'
+]
 
 # load model
-model = joblib.load(path)
-model.predict(df.drop(columns='name'))
+model = load('../get_model/nascar_model.joblib')
+model.predict(df[predictors])
 
 # get predictions
 opt = Optimizer(df)
