bac97f5196843291442c462b212286cee26babfd
wraikes <wraikes@hotmail.com>
2019-07-27 03:59:41 +0000

    Add web driver functionality

--- dfs-nascar/manual_pull.py
+++ dfs-nascar/manual_pull.py
@@ -1,5 +1,10 @@
-import requests
 import pandas as pd, numpy as np
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options, executable_path='geckodriver/geckodriver')
 
+tutorial_soup = BeautifulSoup(driver.page_source, 'html.parser')
 
