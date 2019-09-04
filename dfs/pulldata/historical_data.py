ecb4a7479ee9f0b0d446366db5995ad290a2fbb6
wraikes <wraikes@hotmail.com>
2019-09-04 17:28:47 +0000

    Create main task for entire data pull

--- dfs/pulldata/historical_data.py
+++ dfs/pulldata/historical_data.py
@@ -2,49 +2,49 @@ import json
 import requests
 import boto3
 
-class DataPull:
+class LinestarappData:
     
     dynamodb = boto3.resource('dynamodb')
     parameters = {
-        'nascar': {{
+        'nascar': {
             'sport': 9,
             'pid_start': 209
         },
-        'pga': {{
+        'pga': {
             'sport': 5,
             'pid_start': 112
         },
-        'nhl': {{
+        'nhl': {
             'sport': 6,
             'pid_start': 338
         },
-        'nba': {{
+        'nba': {
             'sport': 2,
             'pid_start': 304
         },
-        'nfl': {{
+        'nfl': {
             'sport': 1,
             'pid_start': 81
-        }
-
-        'mlb': {{
+        },
+        'mlb': {
             'sport': 3,
             'pid_start': 550
         }
     }
     
+    
     def __init__(self, sport, pid_end):
-        self.sport = parameters[sport]['sport']
-        self.pid_star = parameters[sport]['pid_start']
-        
+        self.sport = self.parameters[sport]['sport']
+        self.pid_start = self.parameters[sport]['pid_start']
         self.pid_end = pid_end
-        self.html = 'https://www.linestarapp.com/DesktopModules/DailyFantasyApi/API/Fantasy/GetSalariesV4?sport={}&site=2&periodId='.format(self.sport)
-        self.table = dynamodb.Table('dfs-{}'.format(sport))
         
-
+        self.html = 'https://www.linestarapp.com/DesktopModules/DailyFantasyApi/API/Fantasy/GetSalariesV4?sport={}&site=2&periodId='.format(self.sport)
+        self.table = self.dynamodb.Table('dfs-linestarapp-{}'.format(sport))
+        
+    
     def pull_json_data(self):
 
-        for pid in range(self.start_pid, self.pid_end + 1):
+        for pid in range(self.pid_start, self.pid_end + 1):
             html = self.html + str(pid)
             page = requests.get(html).content.decode() 
             
@@ -58,3 +58,18 @@ class DataPull:
             self.table.put_item(
                Item=data
             )
+
+
+if __name__ == '__main__':
+    sports = {
+        'nascar': 263,
+        'nfl': 133,
+        'mlb': 1310,
+        'nba': 1033,
+        'nhl': 1121,
+        'pga': 222
+    }
+    
+    for key, value in sports.items():
+        data = LinestarappData(key, value)
+        data.pull_json_data()
