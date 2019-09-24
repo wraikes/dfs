8ca79705d5ca0ff2e20654c0cfc065780f67d7a0
wraikes <wraikes@hotmail.com>
2019-09-24 16:28:34 +0000

    Create nascar tables

--- dfs/pulldata/pull_data.py
+++ dfs/pulldata/pull_data.py
@@ -1,11 +1,29 @@
+#todo: Nascar specific, need to scale to other sports 
+    #mark 'projections' for relevant s3 objects in diff sport
+    #find relevant phrasing for sportsline articles in diff sport 
+
 import json
 import requests
 import boto3
+import config
+
+cfg = configparser.ConfigParser()
+cfg.read('pull_data.config')
+user_id = cfg['SPORTSLINE']['user_id']
+password = cfg['SPORTSLINE']['password']
 
 class LinestarappData:
+    '''Class to download Linestarapp Data into S3.
     
+    Parameters:
+        sport: 'nascar', 'nfl', 'nba', 'mlb', 'nhl', or 'pga'
+        site: 'fd' or 'dk'
+    
+    Attributes:
+        
+    '''
     s3 = boto3.resource('s3')
-    bucket = 'my-dfs-data'
+    bucket_name = 'my-dfs-data'
     parameters = {
         'nascar': {
             'sport': 9,
@@ -35,84 +53,88 @@ class LinestarappData:
     
 
     def __init__(self, sport, site='fd'):
+        '''Initialize class'''
         self.sport = sport
-        self.name = site #'fd' or 'dk' only
-        self.site = 2 if site == 'fd' else 1
-        self.sport_id = self.parameters[sport]['sport']
-        self.pid_start = self.parameters[sport]['pid_start']
+        self.site = site
+        
+        self.site_id = 2 if site == 'fd' else 1
+        self.sport_id = self.parameters[self.sport]['sport']
+        self.pid_start = self.parameters[self.sport]['pid_start']
 
-        self.html = 'https://www.linestarapp.com/DesktopModules/DailyFantasyApi/API/Fantasy/GetSalariesV4?sport={}&site={}&periodId='.format(self.sport_id, self.site)
-        self.folder = '{}/linestarapp'.format(sport)
+        self.html = 'https://www.linestarapp.com/DesktopModules/DailyFantasyApi/API/Fantasy/GetSalariesV4?sport={}&site={}&periodId='.format(self.sport_id, self.site_id)
+        self.folder = '{}/linestarapp/'.format(self.sport)
 
+    def _pull_json_data(self, pid):
+        '''Internal method for pulling json data from html page'''
+        html = self.html + str(pid)
+        page = requests.get(html).content.decode() 
+        data = json.loads(page)       
 
-    def pull_historical_data(self, pid_end):
-
-        for pid in range(self.pid_start, pid_end + 1):
-            html = self.html + str(pid)
-            page = requests.get(html).content.decode() 
-            data = json.loads(page)
-            
-            sum_pts = sum([x['PS'] for x in data['Ownership']['Salaries']])
-            if sum_pts != 0:
-                name = '{}/{}_{}.json'.format(self.folder, self.name, pid)
-            else:
-                name = '{}/{}_{}_projections.json'.format(self.folder, self.name, pid)
-            
-            obj = self.s3.Object(self.bucket, name)
-            obj.put(
-                Body=(json.dumps(data))
-            )
-
-            print(pid)
+        return data
 
 
     def update_data(self):
-        bucket = self.s3.Bucket(self.bucket)
-
-        max_pid = 0
+        '''Update missing json data, or update projection data to full data'''
+        bucket = self.s3.Bucket(self.bucket_name)
+        max_pid = self.pid_start
+        
         for obj in bucket.objects.all():
             if self.sport in obj.key and 'json' in obj.key:
+                
+                #update projections only data
                 if 'projections' in obj.key:
 
                     pid = int(obj.key.split('/')[-1].split('.')[0].split('_')[1])
-                    html = self.html + str(pid)
-                    
-                    if pid > max_pid:
+                    data = _pull_json_data(pid)
+                  
+                    if max_pid < pid:
                         max_pid = pid
                     
-                    page = requests.get(html).content.decode()
-                    data = json.loads(page)
+                    #check to see if projections were uploaded
                     sum_pts = sum([x['PS'] for x in data['Ownership']['Salaries']])
-                    
                     if sum_pts == 0:
                         continue
 
-                    new_obj = self.s3.Object(self.bucket, '{}/{}_{}.json'.format(self.folder, self.name, max_pid))
-                    
+                    #save new complete data
+                    new_obj = self.s3.Object(
+                        self.bucket_name,
+                        '{}/{}_{}.json'.format(self.folder, self.site, max_pid)
+                    )
                     new_obj.put(
                         Body=(json.dumps(data))
                     )    
+                    
+                    #delete old projections data
                     self.s3.Object('my-dfs-data', obj.key).delete()
                 
+                #if not projections data, update max pid to record gaps
                 else:
                     pid = int(obj.key.split('/')[-1].split('.')[0].split('_')[1])
                 
                     if max_pid < pid:
                         max_pid = pid
-            
+        
+        #create s3 objects for any missing linestarapp data
         while True:
-            max_pid += 1
-            html = self.html + str(max_pid)
-            page = requests.get(html).content.decode() 
-            if len(page) < 1000:
+            data = _pull_json_data(pid)
+            
+            sum_pts = sum([x['PS'] for x in data['Ownership']['Salaries']])
+            if sum_pts != 0:
+                object_name = '{}/{}_{}.json'.format(self.folder, self.site, pid)
+            else:
+                object_name = '{}/{}_{}_projections.json'.format(self.folder, self.site, pid)
+            
+            #if data is short, it indicates no new data
+            if len(str(data)) < 10000:
                 break
             
-            data = json.loads(page)
-            obj = self.s3.Object(self.bucket, '{}/{}_{}.json'.format(self.folder, self.name, max_pid))
+            #save new complete data
+            obj = self.s3.Object(self.bucket, '{}/{}_{}.json'.format(self.folder, self.site, max_pid))
             obj.put(
                 Body=(json.dumps(data))
             )
 
+            max_pid += 1
             print(max_pid)    
 
 
@@ -121,9 +143,14 @@ class FantasyNerd:
 
 
 class SportsLine:
+    '''Class to download Sportsline articles into S3.
     
+    Parameters:
+        sport: 'nascar', 'nfl', 'nba', 'mlb', 'nhl', or 'pga'
+        
+    '''
     s3 = boto3.resource('s3')
-    bucket = 'my-dfs-data'
+    bucket_name = 'my-dfs-data'
 
     login = 'https://secure.sportsline.com/login'
     payload = {
@@ -133,8 +160,8 @@ class SportsLine:
         'master_product': '23350',
         'vendor': 'sportsline',
         'form_location': 'log_in_page',
-        'userid': '*', 
-        'password': '*'
+        'userid': user_id,
+        'password': password
     }
     
     
@@ -146,6 +173,7 @@ class SportsLine:
     
     
     def _get_links(self):
+        '''Get relevant article links to download.'''
         page = json.loads(requests.get(self.articles).content.decode())
         links = []
         
@@ -160,9 +188,10 @@ class SportsLine:
 
         
     def historical_articles_pull(self):
-
+        '''Pull articles and save to s3 bucket'''
         links = self._get_links()
             
+        #Open session and post the user_id and password
         with requests.Session() as session:
             post = session.post(self.login, data=self.payload)
             
@@ -170,7 +199,8 @@ class SportsLine:
                 article = self.url.format(link)
                 page = session.get(article)
             
-                obj = self.s3.Object(self.bucket, '{}/{}'.format(self.folder, link))
+                #load article into s3 object.
+                obj = self.s3.Object(self.bucket_name, '{}/{}'.format(self.folder, link))
                 obj.put(
                     Body=(page.text)
                 )
@@ -181,22 +211,25 @@ class SportsLine:
     def update_articles(self):
 
         links = self._get_links()
-        bucket = self.s3.Bucket(self.bucket)
+        bucket = self.s3.Bucket(self.bucket_name)
 
         for obj in bucket.objects.all():
             if 'sportsline' in obj.key and self.sport in obj.key:
                 key = obj.key.split('/')[-1]
                 if key in links:
                     links.remove(key)
-        
+                    
+        #Open session and post the user_id and password
         with requests.Session() as session:
             post = session.post(self.login, data=self.payload)
 
+            #open links
             for link in links:
                 article = self.url.format(link)
                 page = session.get(article)
             
-                obj = self.s3.Object(self.bucket, '{}/{}'.format(self.folder, link))
+                #save article into s3 object
+                obj = self.s3.Object(self.bucket_name, '{}/{}'.format(self.folder, link))
                 obj.put(
                     Body=(page.text)
                 )
